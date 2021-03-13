75f7f52fdf01bc815a0bbcd76e039115d2b43ea6
Никита Молодцов <nikita.molodtsov21@gmail.com>
2021-03-13 18:36:56 +0200

    Added basic singleton logic for ConsoleParser

--- image-converter/ConsoleParser.cpp
+++ image-converter/ConsoleParser.cpp
@@ -1,1 +1,19 @@
 #include "ConsoleParser.h"
+
+ConsoleParser::ConsoleParser(std::string pathSource, std::string pathOutput, std::string goalType)
+{
+	pathSource_ = pathSource;
+	pathOutput_ = pathOutput;
+
+	if (goalType == "ppm") goalImageType_ = imageType::PPM;
+	else if (goalType == "bpm") goalImageType_ = imageType::BPM;
+	else if (goalType == "gif") goalImageType_ = imageType::GIF;
+	else if (goalType == "png") goalImageType_ = imageType::PNG;
+	else if (goalType == "jpeg") goalImageType_ = imageType::JPEG;
+	else goalImageType_ = imageType::UNKNOWN;
+}
+
+ConsoleParser* ConsoleParser::GetInstance(const std::string& value)
+{
+	
+}

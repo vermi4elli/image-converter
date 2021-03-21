8edf8ec94510c6b6a36aa28948fb51a2a2c359fc
OlexandrBebeh <olexandrbebeh@gmail.com>
2021-03-21 23:46:59 +0200

    Add deflate

---
+++ image-converter/ImageReaderImplementations/png/Deflate.cpp
@@ -0,0 +1,23 @@
+#include "Deflate.h";
+#include <fstream>;
+#include <iomanip>;
+#include <algorithm>;
+#include <vector>;
+#include <fstream>
+#include <iostream>
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
+using namespace boost::iostreams;
+
+void Deflate::compress(std::stringbuf& buf, filtering_streambuf<boost::iostreams::input>& in) {
+
+	in.push(zlib_compressor());
+	in.push(buf);
+};
+
+void  Deflate::decompress(std::stringbuf& buf, filtering_streambuf<boost::iostreams::input>& in) {
+	in.push(zlib_decompressor());
+	in.push(buf);
+
+};

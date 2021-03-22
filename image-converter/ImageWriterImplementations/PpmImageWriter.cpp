a91c1e47b9d4e3e9ecc13c4eb2c1316ccd4c0c95
Никита Молодцов <nikita.molodtsov21@gmail.com>
2021-03-22 23:16:26 +0200

    Optimized PPM writer

--- image-converter/ImageWriterImplementations/PpmImageWriter.cpp
+++ image-converter/ImageWriterImplementations/PpmImageWriter.cpp
@@ -4,16 +4,20 @@ void PpmImageWriter::write(std::vector<std::vector<RGBAquad>> data)
 {
 	std::ofstream fout(name);
     int W = data.front().size(), H = data.size(), max = data.front().front().a;
-    
-    fout << "P3\n" << W << " " << H << "\n" << max << "\n";
+    constexpr char SPACE = ' ';
 
+    fout << "P3\n" << W << SPACE << H << "\n" << max << "\n";
+    
     for (int i = 0; i < H; i++)
     {
         for (int j = 0; j < W; j++)
         {
-            fout << (int)data[i][j].r << " ";
-            fout << (int)data[i][j].g << " ";
-            fout << (int)data[i][j].b << " ";
+            fout.write(std::to_string((int)data[i][j].r).c_str(), std::to_string((int)data[i][j].r).length());
+            fout.put(SPACE);
+            fout.write(std::to_string((int)data[i][j].g).c_str(), std::to_string((int)data[i][j].g).length());
+            fout.put(SPACE);
+            fout.write(std::to_string((int)data[i][j].b).c_str(), std::to_string((int)data[i][j].b).length());
+            fout.put(SPACE);
         }
     }
 

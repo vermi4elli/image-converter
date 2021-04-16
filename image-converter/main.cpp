adc4222460e675ad76f54f76b92b8ba3b082c300
Никита Молодцов <nikita.molodtsov21@gmail.com>
2021-04-17 01:43:15 +0300

    Updated .gitignore

--- image-converter/main.cpp
+++ image-converter/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]) {
 		ConsoleParser* consoleParser = ConsoleParser::GetInstance(argc, argv);
 		DI.set<ConsoleParser*>(consoleParser);
 
-		OBJParser* objParser = OBJParser::GetInstance("cow.obj");
+		OBJParser* objParser = OBJParser::GetInstance("simplecow.obj");
 		DI.set<OBJParser*>(objParser);
 		DI.set<std::vector<FigureI*>>((*(DI.get<OBJParser*>()))->GetFaces().begin(), (*(DI.get<OBJParser*>()))->GetFaces().end());
 
